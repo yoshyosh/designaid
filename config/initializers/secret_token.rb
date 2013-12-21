7d4af8eadb141075f24e85ca898c972583011940
Joseph Anderson <janderson760@gmail.com>
2013-12-21 11:42:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Designaid::Application.config.secret_token = '9a0ec76adfdd0c4ceac3ba9f211e04813a4dab17fe7ddf6bda36c7dcb64d96eb0a20a1b5cf4416e958af65c0379a390b390e0fecc2fdadb15f5645839a6bf191'
